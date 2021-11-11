a0f4426b23ccc819e11d522af60ac78d834fe02b
Liew Kuan Yung <liewkuanyung@gmail.com>
2021-11-11 20:27:44 +0800

    completed tutorial-8

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -6,6 +6,12 @@ import {
     Link, 
     Redirect 
 } from "react-router-dom"
+import { 
+    TextField, 
+    Button, 
+    Grid, 
+    Typography 
+} from "@material-ui/core";
 
 import RoomJoinPage from "./RoomJoinPage";
 import RoomCreatePage from "./RoomCreatePage";
@@ -22,7 +28,31 @@ export default class HomePage extends Component{
             <Router>
                 <Switch>
                     <Route exact path="/">
-                        <p>This is the home page</p>
+                        <Grid container spacing={1}>
+                            <Grid item xs={12} align="center">
+                                <Typography variant="h4" component="h4">
+                                    <p>Welcom Home</p>
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={12} align="center">
+                                <Button 
+                                    variant="contained"
+                                    color="secondary"
+                                    href="/create"
+                                >
+                                    Create New Room!
+                                </Button>
+                            </Grid>
+                            <Grid item xs={12} align="center">
+                                <Button 
+                                    variant="contained"
+                                    color="primary"
+                                    href="/join"
+                                >
+                                    Join Room
+                                </Button>
+                            </Grid>
+                        </Grid>
                     </Route>
                     <Route path="/join" component={RoomJoinPage} />
                     <Route path="/create" component={RoomCreatePage} />
